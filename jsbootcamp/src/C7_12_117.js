64e5621a604f63842d24e415df8c081ea503f42a
Michael Giebner <mikegiebner@gmail.com>
2019-11-29 17:43:57 -0500

    C7:12:117 re-do

--- jsbootcamp/src/C7_12_117.js
+++ jsbootcamp/src/C7_12_117.js
@@ -10,21 +10,41 @@ class C7_12_117 extends Component{
          const min = Math.min(...arr)
          const sum = arr.reduce((sum, r) => sum + r)
          const avg = sum / arr.length
+         const ran = Math.random()
+         const pi  = Math.PI
 
          return {
             max,
             min,
             sum,
-            avg
+            avg,
+            ran,
+            pi
          }
       }
 
       const stats = getStats(reviews)
 
       const { max, min, sum, avg } = stats
+      const key = Object.keys(stats)
+      const val = Object.values(stats)
+      const display = []
+
+      for(var i = 0; i < key.length; i++){
+         display.push(<h1>{`${key[i]}: ${val[i]}`}</h1>)
+      }
 
       return(
-         <h1> { `max: ${max}, min: ${min}, sum: ${sum}, avg: ${avg}` } </h1>
+         // <h1> { `max: ${max}, min: ${min}, sum: ${sum}, avg: ${avg}` } </h1>
+         // <div>
+         //    <h1>{`${key[0]}: ${val[0]}`}</h1>
+         //    <h2>{`${key[1]}: ${val[1]}`}</h2>
+         //    <h3>{`${key[2]}: ${val[2]}`}</h3>
+         //    <h4>{`${key[3]}: ${val[3]}`}</h4>
+         // </div>
+         <div>
+            {display}
+         </div>
       )
    }
 }
